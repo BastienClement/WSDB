91735fe3fd1d25e4e13b4b73d1a853b62d6f9c6f
Bastien Clément <g@ledric.me>
2016-01-10 01:13:47 +0100

    Collection list

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,30 +3,39 @@ package controllers
 import controllers.mysql._
 import play.api.mvc._
 
-case class Deck(id: Int, name: String, universes: Seq[String], ncx_count: Int, cx_count: Int)
+case class Deck(id: Int, name: String, universes: Seq[(String, String)], ncx_count: Int, cx_count: Int)
 
 class Application extends Controller {
-	def index = UserAction { implicit req =>
-		Ok(views.html.index())
+	def index = UserAction.async { implicit req =>
+		sql"""
+			SELECT a.id, u.id, u.name,
+				(SELECT COUNT(*) FROM extensions AS e WHERE e.universe = u.id),
+				(SELECT COUNT(*) FROM cards AS c JOIN extensions AS e ON e.id = c.extension WHERE e.universe = u.id)
+			FROM universes AS u
+				JOIN aspects AS a ON a.id = u.aspect
+			ORDER BY a.id ASC, u.name ASC
+	  """.as[(String, String, String, Int, Int)].run.map { universes =>
+			Ok(views.html.index(universes))
+		}
 	}
 
 	def decks = Authenticated.async { implicit req =>
 		val user = req.user.name
 		sql"""
-			SELECT DISTINCT d.id, d.name, dc.universe_name,
+			SELECT DISTINCT d.id, d.name, dc.universe, dc.universe_name,
 				(SELECT COALESCE(SUM(quantity), 0) FROM deck_cards
-				 WHERE deck_id = d.id AND universe = dc.universe AND type != 'CX') AS ncx,
-				(SELECT COALESCE(SUM(quantity), 0) FROM deck_cards WHERE deck_id = d.id AND universe = dc.universe AND type = 'CX') AS cx
+				 WHERE deck_id = d.id AND universe = dc.universe AND type != 'CX'),
+				(SELECT COALESCE(SUM(quantity), 0) FROM deck_cards WHERE deck_id = d.id AND universe = dc.universe AND type = 'CX')
 			FROM decks AS d
 			LEFT JOIN deck_cards AS dc ON dc.deck_id = d.id
 			WHERE d.user = $user
 			ORDER BY d.name ASC, dc.universe_name ASC
-		""".as[(Int, String, Option[String], Int, Int)].run map { case rows =>
+		""".as[(Int, String, Option[String], Option[String], Int, Int)].run.map { case rows =>
 			rows.pack(r => (r._1, r._2)).map { case data =>
-				val universes = data.collect { case (_, _, u, _, _) if u.isDefined => u.get }
-				Deck(data.head._1, data.head._2, universes, data.map(_._4).sum, data.map(_._5).sum)
+				val universes = data.collect { case (_, _, uid, uname, _, _) if uid.isDefined => (uid.get, uname.get) }
+				Deck(data.head._1, data.head._2, universes, data.map(_._5).sum, data.map(_._6).sum)
 			}
-		} map {
+		}.map {
 			case decks => Ok(views.html.decks(decks))
 		}
 	}
