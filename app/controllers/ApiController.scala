6dbdc1244f48f03fd9b9e879e6925da09e0d1021
galedric <g@ledric.me>
2016-01-14 15:14:16 +0100

    Rarity

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -9,17 +9,18 @@ class ApiController extends Controller {
 		val ext = req.queryString.get("id").get.head
 		val user = if (req.authenticated) req.user.name else ""
 		sql"""
-			SELECT c.identifier, c.name, c.color, IFNULL(col.quantity, 0)
+			SELECT c.identifier, c.name, c.color, c.rarity, IFNULL(col.quantity, 0)
 			FROM complete_cards AS c
 			LEFT JOIN collections AS col ON col.card = c.id AND col.version = c.version AND col.user = $user
 			WHERE extension_id = $ext
 			ORDER BY identifier ASC
-		""".as[(String, String, String, Int)].run.map { rows =>
-			Json.toJson(rows.map { case (id, name, color, quantity) =>
+		""".as[(String, String, String, String, Int)].run.map { rows =>
+			Json.toJson(rows.map { case (id, name, color, rarity, quantity) =>
 				Json.obj(
 					"id" -> id,
 					"name" -> name,
 					"color" -> color,
+					"rarity" -> rarity,
 					"quantity" -> quantity
 				)
 			})
