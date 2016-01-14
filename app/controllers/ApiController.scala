44922d30a4ddb4bd48775482ac8d3211850c2396
galedric <g@ledric.me>
2016-01-14 14:13:01 +0100

    Card colors

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -9,17 +9,18 @@ class ApiController extends Controller {
 		val ext = req.queryString.get("id").get.head
 		val user = if (req.authenticated) req.user.name else ""
 		sql"""
-			SELECT c.identifier, c.name, IFNULL(col.quantity, 0)
+			SELECT c.identifier, c.name, c.color, IFNULL(col.quantity, 0)
 			FROM complete_cards AS c
 			LEFT JOIN collections AS col ON col.card = c.id AND col.version = c.version AND col.user = $user
 			WHERE extension_id = $ext
 			ORDER BY identifier ASC
-		""".as[(String, String, Int)].run.map { rows =>
-			Json.toJson(rows.map { case (id, name, owned) =>
+		""".as[(String, String, String, Int)].run.map { rows =>
+			Json.toJson(rows.map { case (id, name, color, quantity) =>
 				Json.obj(
 					"id" -> id,
 					"name" -> name,
-					"quantity" -> owned
+					"color" -> color,
+					"quantity" -> quantity
 				)
 			})
 		}.map { array =>
